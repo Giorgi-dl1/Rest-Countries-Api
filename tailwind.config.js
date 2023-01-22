dc02011741ca4bd2de4b70fb011f06a0da97bd81
Giorgi-dl1 <96057077+Giorgi-dl1@users.noreply.github.com>
2023-01-23 00:42:41 +0400

    add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
@@ -7,6 +8,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'dark-elements': '#2b3945',
+      'dark-bg': '#202c37',
+      'light-text': '#111517',
+      'light-input': '#858585',
+      'light-bg': '#fafafa',
+      'light-elements': '#ffffff',
+      'dark-text': '#ffffff',
+    },
   },
   plugins: [],
-}
+};

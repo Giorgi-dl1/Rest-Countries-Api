af238061ad2d9be82bb8ff1fc6a09b054c33e9e5
Giorgi <stichy.dl@gmail.com>
2023-01-23 22:01:16 +0400

    create loading state

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,41 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Layout from '../components/Layout';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Layout from '../components/Layout'
+import Router from 'next/router'
+import Loading from '../components/Loading'
+import { useEffect, useState } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    const start = () => {
+      console.log('start')
+      setLoading(true)
+    }
+    const end = () => {
+      console.log('finished')
+      setLoading(false)
+    }
+    Router.events.on('routeChangeStart', start)
+    Router.events.on('routeChangeComplete', end)
+    Router.events.on('routeChangeError', end)
+    return () => {
+      Router.events.off('routeChangeStart', start)
+      Router.events.off('routeChangeComplete', end)
+      Router.events.off('routeChangeError', end)
+    }
+  }, [])
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
+    </>
+  )
 }
 
-export default MyApp;
+export default MyApp
